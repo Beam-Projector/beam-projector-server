3e44b41dad43317ddd46d6341ee0feff41f17fd4
dabinHam <113006964+dabinHam@users.noreply.github.com>
2023-07-15 02:21:05 +0900

    ✨: 초기 세팅
    Spring MVC 패턴과 의존성 추가 완료

---
+++ beam-projector/src/main/java/com/projet/beamprojector/BeamProjectorApplication.java
@@ -0,0 +1,13 @@
+package com.projet.beamprojector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeamProjectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeamProjectorApplication.class, args);
+	}
+
+}
