953217e0438bf2f1c29604a39df7a0510e327642
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-07-24 00:19:24 +0900

    Feat : Spring Security 초기 설정

---
+++ beam-projector/src/main/java/com/projet/beamprojector/config/Security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.projet.beamprojector.config.Security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final CustomAuthenticationProvider customAuthenticationProvider;
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http)
+		throws Exception {
+		http
+			.authorizeRequests()
+			.antMatchers("/api/login").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin().loginProcessingUrl("/api/login")
+			.and()
+			.logout().logoutUrl("/api/logout");
+
+		// 커스텀 인증 제공자를 등록합니다.
+		http.authenticationProvider(customAuthenticationProvider);
+
+		return http.build();
+	}
+}
