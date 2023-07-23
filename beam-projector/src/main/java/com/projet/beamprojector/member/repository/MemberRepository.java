57b1dc81cf32bbf1edee936e4afb149c16733c60
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-07-24 02:26:51 +0900

    Feat : MemberRepsotiroty 생성, (아이디, 이메일, 닉네임) 중복 조회 기능 추가

---
+++ beam-projector/src/main/java/com/projet/beamprojector/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.projet.beamprojector.member.repository;
+
+import com.projet.beamprojector.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+
+	boolean existsByEmail(String email);
+	boolean existsByMemberId(String memberId);
+	boolean existsByNickName(String nickName);
+
+
+}
