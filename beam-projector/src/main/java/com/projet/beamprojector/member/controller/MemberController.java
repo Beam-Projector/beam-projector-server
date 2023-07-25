8ea9da62afe2be19026558b69bbcde174959dce5
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-07-26 04:24:15 +0900

    Refactor : API 명세서에 맞게 controller 분리 MemberController, AuthController로 분리

--- beam-projector/src/main/java/com/projet/beamprojector/member/controller/MemberController.java
+++ beam-projector/src/main/java/com/projet/beamprojector/member/controller/MemberController.java
@@ -1,15 +1,12 @@
 package com.projet.beamprojector.member.controller;
 
-import com.projet.beamprojector.domain.entity.Member;
-import com.projet.beamprojector.dto.member.MemberDto;
+import com.projet.beamprojector.config.Security.TokenMemberId;
 import com.projet.beamprojector.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,22 +18,12 @@ public class MemberController {
 
 	private final MemberService memberService;
 
-	@Operation(summary = "회원 가입 신청", description = "회원 가입 정보를 받아 회원을 등록합니다.")
-	@PostMapping("/signup")
-	public ResponseEntity<?> signup(
-		@Validated @RequestBody MemberDto.SignupRequest request) {
-		log.info("signup request => {}", request);
-		Member member = memberService.registerMember(request);
-		return ResponseEntity.ok(member.getMemberId()
-			+ "님의 회원가입이 완료 되었습니다! 로그인 후 서비스를 사용하실 수 있습니다.");
+	@Operation(summary = "회원정보 조회", description = "JWT를 받고 회원 정보를 반환 합니다.")
+	@PostMapping("/")
+	public ResponseEntity<?> getMember(@TokenMemberId String memberId) {
+		log.info("memberId => {}", memberId);
+		return ResponseEntity.ok(200);
 	}
 
-	@Operation(summary = "로그인", description = "아이디와 비밀번호를 입력받아 JWT를 반환 합니다.")
-	@PostMapping("/login")
-	public ResponseEntity<?> login(
-		@Validated @RequestBody MemberDto.LoginRequest request) {
-		log.info("login request => {}", request.getMemberId());
-		String token = memberService.login(request);
-		return ResponseEntity.ok(token);
-	}
+
 }
