c797e834925a6c7c5a4e39682b2c6afc3db4370c
dabin <113006964+dabinHam@users.noreply.github.com>
2023-07-28 03:53:23 +0900

    Refactor :Comment JWT 추가로 기존 Member 변경
    - 기존 MemberRepository를 통하여 불러왔던 memberNum을 JWT를 추가면서 변경하며 dto에 담겨있던 member정보 삭제.
    - 작성자와 현재 이용자가 구분이 되지 않아 변수명 변경 처리
    - 필요 없는 주석 및 불필요한 부분 삭제 처리

--- beam-projector/src/main/java/com/projet/beamprojector/comment/service/CommentService.java
+++ beam-projector/src/main/java/com/projet/beamprojector/comment/service/CommentService.java
@@ -12,13 +12,10 @@ import com.projet.beamprojector.dto.comment.CommentDto;
 import com.projet.beamprojector.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Service
@@ -27,28 +24,20 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final MemberRepository memberRepository;
-    private final BoardRepository boardRepository; // 생성되면 주석해제 후 작업 시작
+    private final BoardRepository boardRepository;
 
     // 생성
-    public Comment createComment(CommentDto.CreateCommentRequest createCommentRequest){
+    public Comment createComment(CommentDto.CreateCommentRequest createCommentRequest, String memberId) {
         log.info("서비스 진입성공!!!");
-        log.info("CommentDto ,createCommentRequest : {}", createCommentRequest.getMemberNum());
-        log.info("createCommentRequest Type: {}", createCommentRequest.getMemberNum());
-        Member member = memberRepository.findByMemberId(String.valueOf(createCommentRequest.getMemberNum()));
-        log.info("Service!!!!!! >>>>>>>>>>>>>>>>> :{}",createCommentRequest.getMemberNum());
+
+        Member member = memberRepository.findByMemberId(memberId);
         Board board = boardRepository.findById(createCommentRequest.getBoardNum()).orElse(null);
 
-        // jwt : 토큰 없으면 빠이 !
-        // jwt로 1차적인 글쓰기 버튼눌렀을때는 검증ok
-        // -> 2차로 저장버튼 눌렀을 시 로그인상태인지 확인 하기 (중요)
-
-        // if 분기처리해야함 board , member 없는경우
-        if(board == null || member == null){
+        if (board == null || member == null) {
             throw new CommentException(
-                    CommentErrorCode.DUPLICATED_COMMENT_CREATE);
+                    CommentErrorCode.COMMENT_MEMBER_ONLY_ALLOWED);
         }
 
-        // set 직전에 꼭 검증 하기!!
         LocalDateTime createTime = LocalDateTime.now();
         Comment comment = new Comment();
         comment.setBoard(board);
@@ -56,79 +45,55 @@ public class CommentService {
         comment.setCreateAt(createTime);
         comment.setMember(member);
 
-
         return commentRepository.save(comment);
     }
 
     // 조회
     public List<CommentDto.CommentResponse> getComment(Long boardNum) {
-        log.info("boardNum Type: {}",boardNum);
-        log.info("commentRepository : {}",commentRepository.findAllByBoardId(boardNum).toString());
-        List<Comment>comments = commentRepository.findAllByBoardId(boardNum);
-//        return commentRepository.findAllByBoardId(boardNum);
+        log.info("boardNum Type: {}", boardNum);
+        log.info("commentRepository : {}", commentRepository.findAllByBoardId(boardNum).toString());
+        List<Comment> comments = commentRepository.findAllByBoardId(boardNum);
         return CommentDto.CommentResponse.toResponse(comments);
     }
 
     // 수정
-    public CommentDto.CommentResponse updateComment(CommentDto.UpdateCommentRequest updateCommentRequest) {
-        log.info("수정 서비스 진입!111");
-//        Optional<Comment> comment = commentRepository.findById(updateCommentRequest.getCommentNum());
-        log.info("수정 서비스 진입!222");
+    public CommentDto.CommentResponse updateComment(
+            String memberId,
+            CommentDto.UpdateCommentRequest updateCommentRequest) {
         Comment comment = commentRepository.findById(updateCommentRequest.getCommentNum())
-                .orElseThrow(() -> new CommentException(CommentErrorCode.DUPLICATED_COMMENT_BASIC));
+                .orElseThrow(() -> new CommentException(CommentErrorCode.COMMENT_INVALID_ACCESS));
 
-        // jwt를 통해서 지금 사용자와 댓글의 사용자를 비교 가능하게 함.
-        String member = comment.getMember().getMemberId();
+        // writer : 작성자
+        String writer = comment.getMember().getMemberId();
 
+        if (!writer.equals(memberId)) {
+            throw new CommentException(CommentErrorCode.COMMENT_INVALID_ACCESS);
+        }
 
         LocalDateTime modifiedTime = LocalDateTime.now();
         comment.setComments(updateCommentRequest.getComments());
         comment.setModifiedAt(modifiedTime);
 
-
-        // 검증은 무조건 수정 앞에..!
         commentRepository.save(comment);
 
-/*        if (optionalComment.isPresent()) {
-            // 댓글이 존재하는 경우, 해당 댓글 가져오기
-//            Comment comment = optionalComment.get();
-
-            // commentResponse객체에서 새로운 댓글내용 가져와서 엔티티에 설정
-            comment.setComments(comment.getComments());
-
-            // 댓글 수정시간 변경
-            comment.setModifiedAt(LocalDateTime.now());
-
-            // 댓글 엔티티 DB저장 -> 업데이트 댓글 받아옴
-            comment = commentRepository.save(comment);
-
-            // 업데이트 댓글 -> CommentDto.CommentResponse로 반환
-            return CommentDto.CommentResponse.toResponse(comment);
-        }*/
-
         log.info("수정 완료");
-        // 댓글이 없으면 아무것도 하지 않음.
         return CommentDto.CommentResponse.toResponse(comment);
     }
 
     // 삭제
-    public void deleteComment(Long commentNum) {
-//        Long commentNum = deleteCommentRequest.getCommentNum();
-//        Long memberNum = deleteCommentRequest.getMemberNum();
-
-        // 회원권한 확인 -> 삭제작업 처리
-//        Member member = memberRepository.findById(String.valueOf(memberNum))
-//                .orElseThrow(() -> new CommentException(CommentErrorCode.DUPLICATED_COMMENT_BASIC));
-        log.info("삭제 서비스 들어왔다");
-        log.info("11 commentNum Type: {}",commentNum);
+    public void deleteComment(Long commentNum, String memberId
+    ) {
         Comment comment = commentRepository.findById(commentNum)
-                .orElseThrow(() -> new CommentException(CommentErrorCode.DUPLICATED_COMMENT_BASIC));
-        log.info("22 commentNum Type: {}",commentNum);
-//        if(!comment.getMember().getId().equals(memberNum)){
-//            throw new ResponseStatusException(HttpStatus.FORBIDDEN, CommentErrorCode.DUPLICATED_COMMENT_DELETE_FALSE.toString());
-//        }
+                .orElseThrow(() -> new CommentException(CommentErrorCode.COMMENT_INVALID_ACCESS));
 
+        String writer = comment.getMember().getMemberId();
 
         commentRepository.deleteById(commentNum);
+
+        if (!writer.equals(memberId)) {
+            throw new CommentException(CommentErrorCode.COMMENT_DELETE_FAILED);
+        } else {
+            throw new CommentException(CommentErrorCode.COMMENT_DELETE_SUCCESS);
+        }
     }
 }
--- beam-projector/src/main/java/com/projet/beamprojector/dto/comment/CommentDto.java
+++ beam-projector/src/main/java/com/projet/beamprojector/dto/comment/CommentDto.java
@@ -1,19 +1,15 @@
 package com.projet.beamprojector.dto.comment;
 
-import com.projet.beamprojector.domain.entity.Board;
 import com.projet.beamprojector.domain.entity.Comment;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CommentDto {
-
-    // DTO : 데이터를 전달 응답,요청 할때 사용하는 폼
 
     @ToString
     @Builder
@@ -21,19 +17,16 @@ public class CommentDto {
     @Setter
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class CreateCommentRequest{
+    public static class CreateCommentRequest {
         @Schema(description = "게시글번호")
         private Long boardNum;
         @Schema(description = "회원번호")
         private Long memberNum;
-
         @NotBlank(message = "내용을 입력해 주세요.")
         @Schema(description = "댓글 입력란")
         private String comments;
-
-
         @Schema(description = "작성시간")
-        private LocalDateTime createAt ;
+        private LocalDateTime createAt;
 
     }
 
@@ -42,7 +35,7 @@ public class CommentDto {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class UpdateCommentRequest{
+    public static class UpdateCommentRequest {
         @NotBlank(message = "내용을 입력해 주세요.")
         @Schema(description = "댓글 입력란")
         private String comments;
@@ -50,22 +43,7 @@ public class CommentDto {
         private Long boardNum;
         @Schema(description = "댓글번호")
         private Long commentNum;
-        @Schema(description = "회원번호")
-        private Long memberNum;
     }
-
-
-//    @Builder
-//    @Getter
-//    @Setter
-//    @NoArgsConstructor
-//    @AllArgsConstructor
-//    public static class DeleteCommentRequest {
-//        @Schema(description = "댓글번호")
-//        private Long commentNum;
-//        @Schema(description = "회원번호")
-//        private Long memberNum;
-//    }
 
     @ToString
     @Builder
@@ -73,11 +51,8 @@ public class CommentDto {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class CommentResponse{
-        // 추가적으로 더 설정해줘야함.
-        // 조회 반환타입
-        @Schema(description = "회원번호")
-        private Long memberNum;
+    public static class CommentResponse {
+
         @Schema(description = "게시글번호")
         private Long boardNum;
         @Schema(description = "댓글번호")
@@ -91,11 +66,10 @@ public class CommentDto {
         private String comments;
 
         @Schema(description = "작성시간")
-        private LocalDateTime createAt ;
+        private LocalDateTime createAt;
 
-        public static CommentResponse toResponse(Comment comment){
+        public static CommentResponse toResponse(Comment comment) {
             return CommentResponse.builder()
-                    .memberNum(comment.getMember().getId())
                     .boardNum(comment.getBoard().getId())
                     .commentNum(comment.getId())
                     .nickName(comment.getMember().getNickName())
@@ -104,7 +78,7 @@ public class CommentDto {
                     .build();
         }
 
-        public static List<CommentResponse> toResponse(List<Comment> list){
+        public static List<CommentResponse> toResponse(List<Comment> list) {
             return list.stream().map(CommentResponse::toResponse).collect(Collectors.toList());
         }
     }
