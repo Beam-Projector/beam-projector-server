23be6f1c315c40e774973f53b4bc58ae79df7357
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-07-29 05:38:35 +0900

    Refactor : MemberException 수정 토큰 관련 예외 추가

--- beam-projector/src/main/java/com/projet/beamprojector/member/exception/type/MemberErrorCode.java
+++ beam-projector/src/main/java/com/projet/beamprojector/member/exception/type/MemberErrorCode.java
@@ -11,6 +11,12 @@ public enum MemberErrorCode {
 	DUPLICATED_MEMBER_NICKNAME("중복된 닉네임 입니다!"),
 	PASSWORD_MISMATCH("비밀 번호가 일치하지 않습니다"),
 	MEMBER_NOT_FOUND("등록 되지 않은 아이디 입니다"),
+
+	EXPIRED_ACCESS_TOKEN("만료된 토큰 입니다"),
+	UNSUPPORTED_TOKEN("지원하지 않는 토큰 입니다"),
+	AUTHORIZATION_HEADER_NOT_EMPTY("인증 헤더 값이 비어있습니다"),
+
+
 	;
 	private final String description;
 }
