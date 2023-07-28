4668a66aa204f5f52569b3a5ce38ebc88bd11d54
Joowon-Seo <tjwndnjs1998@gmail.com>
2023-07-29 04:21:06 +0900

    Feat : GlobalExceptionHandler 구현 예외들을 유형화 하고 clinet에 ErrorCode와 ErrorMessage를 보냅니다.

---
+++ beam-projector/src/main/java/com/projet/beamprojector/exception/type/ErrorCode.java
@@ -0,0 +1,13 @@
+package com.projet.beamprojector.exception.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+	BAD_REQUEST("입력 값을 확인해 주세요!");
+
+	private final String description;
+
+}
