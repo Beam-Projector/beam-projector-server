4198275f0c8796497709b789154b3b77baee52bd
dabin <113006964+dabinHam@users.noreply.github.com>
2023-07-29 01:24:01 +0900

    Refactor :BoardDTO CamelCase 적용 및 어노테이션 추가, 주석정리
    BoardDTO ->BoardDto
    - CamelCase적용하여 명칭 수정
    
    - swagger 어노테이션 적용.
    - 주석 및 코드 삭제
    - JWT로 대체된 memberId 삭제
    - boardHits 추가

--- beam-projector/src/main/java/com/projet/beamprojector/dto/board/BoardDTO.java
+++ beam-projector/src/main/java/com/projet/beamprojector/dto/board/BoardDTO.java
@@ -1,99 +1,63 @@
 package com.projet.beamprojector.dto.board;
 
-//data transfer object
-
 import com.projet.beamprojector.domain.entity.Board;
-import com.projet.beamprojector.domain.entity.Comment;
-import com.projet.beamprojector.dto.comment.CommentDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
-
-public class BoardDTO {
-//    private Long id;
-////    private String memberId;
-//    private String title;
-//    private String content;
-//    private int boardHits;
-////    private LocalDateTime createAt;
-////    private String modifiedAt;
-//    private Long categoryName;
-//    private String disclosure;
-//
-//    public BoardDTO(Long id,  String title, int boardHits, LocalDateTime createAt) {
-//        this.id = id;
-//
-//        this.title = title;
-//        this.boardHits = boardHits;
-////        this.createAt = createAt;
-    //}
-
-
-
-//    public static BoardDTO toBoardDTO(Board board) {
-//        BoardDTO boardDTO = new BoardDTO();
-//        boardDTO.setId(boardDTO.getId());
-//        boardDTO.setTitle(boardDTO.getTitle());
-////        boardDTO.setMemberId(boardDTO.getMemberId());
-//        boardDTO.setContent(boardDTO.getContent());
-//        boardDTO.setCategoryName(board.getCategoryName());
-//        boardDTO.setDisclosure(board.getDisclosure());
-//        boardDTO.setBoardHits(board.getBoardHits());
-////        boardDTO.setCreateAt(board.getCreateAt());
-////        boardDTO.setCreateAt(board.getModifiedAt());
-//        return boardDTO;
-//
-//    }
-
+public class BoardDto {
     @ToString
     @Builder
     @Getter
     @Setter
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class CreateBoardRequest{
+    public static class CreateBoardRequest {
+
+        @NotBlank(message = "제목을 입력해 주세요.")
+        @Schema(description = "제목 입력란")
         private String title;
+        @NotBlank(message = "내용을 입력해 주세요.")
+        @Schema(description = "내용 입력란")
         private String content;
+
+        @Schema(description = "공개범위")
         private String disclosure;
+
+        @Schema(description = "카테고리 분류번호")
         private Long categoryName;
-
-
-//        @NotBlank(message = "내용을 입력해 주세요.")
-//        @Schema(description = "댓글 입력란")
-//        private String comments;
-//
-//
-//        @Schema(description = "작성시간")
-//        private LocalDateTime createAt ;
-
     }
+
     @ToString
     @Builder
     @Getter
     @Setter
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class UpdateBoardRequest{
+    public static class UpdateBoardRequest {
+
+        @Schema(description = "게시글번호")
         private Long boardNum;
+
+        @NotBlank(message = "제목을 입력해 주세요.")
+        @Schema(description = "제목 입력란")
         private String title;
+        @NotBlank(message = "내용을 입력해 주세요.")
+        @Schema(description = "내용 입력란")
         private String content;
+
+        @Schema(description = "공개범위")
         private String disclosure;
+
+        @Schema(description = "카테고리 분류번호")
         private Long categoryName;
 
-
-//        @NotBlank(message = "내용을 입력해 주세요.")
-//        @Schema(description = "댓글 입력란")
-//        private String comments;
-//
-//
-//        @Schema(description = "작성시간")
-//        private LocalDateTime createAt ;
-
     }
+
     @ToString
     @Builder
     @Getter
@@ -101,39 +65,46 @@ public class BoardDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class BoardResponse {
+
+        @Schema(description = "게시글번호")
         private Long boardNum;
-        private String memberId;
+        @NotBlank(message = "제목을 입력해 주세요.")
+        @Schema(description = "제목 입력란")
         private String title;
+        @NotBlank(message = "내용을 입력해 주세요.")
+        @Schema(description = "내용 입력란")
         private String content;
+        @Schema(description = "공개범위")
         private String disclosure;
+
+        @Schema(description = "게시글 조회수")
+        private Long boardHits;
+
+        @Schema(description = "카테고리 분류번호")
         private Long categoryName;
+
+        @Schema(description = "작성시간")
         private LocalDateTime createAt;
+        @Schema(description = "수정시간")
         private LocalDateTime modifiedAt;
 
-        public static BoardResponse toResponse(Board board){
+        public static BoardResponse toResponse(Board board) {
             return BoardResponse.builder()
-                    .memberId(board.getMember().getMemberId())
                     .boardNum(board.getId())
                     .title(board.getTitle())
                     .createAt(board.getCreateAt())
                     .content(board.getContent())
+                    .boardHits(board.getBoardHits())
                     .categoryName(board.getCategoryName())
                     .disclosure(board.getDisclosure())
                     .modifiedAt(board.getModifiedAt())
                     .build();
         }
-        public static List<BoardDTO.BoardResponse> toResponse(List<Board> list){
-            return list.stream().map(BoardDTO.BoardResponse::toResponse).collect(Collectors.toList());
+
+        public static List<BoardDto.BoardResponse> toResponse(List<Board> list) {
+            return list.stream().map(BoardDto.BoardResponse::toResponse).collect(Collectors.toList());
         }
 
-
-//        @NotBlank(message = "내용을 입력해 주세요.")
-//        @Schema(description = "댓글 입력란")
-//        private String comments;
-//
-//
-//        @Schema(description = "작성시간")
-//        private LocalDateTime createAt ;
 
     }
 
